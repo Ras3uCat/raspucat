b99033ddb6ab59b7e822fe9f449df69a5aa73871
rmr32 <rmr@gmx.us>
2026-02-22 11:06:09 -0600

    legal link

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,10 @@
+import 'package:raspucat/app/modules/screens/bingequest_legal_screen.dart';
 import 'package:raspucat/utils/constants/exports.dart';
 
 class AppRoutes {
   static final pages = [
     GetPage(name: ERoutes.home, page: () => EResponsiveScreen()),
+    GetPage(name: ERoutes.bingeQuestLegal, page: () => const BingeQuestLegalScreen()),
   ];
 }
 
