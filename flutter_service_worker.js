4fcf9e9fc9cba05c8ac02a6794aeb1d365cef7fd
Ras3uCat <Ras3uCat@users.noreply.github.com>
2026-05-14 17:42:12 +0000

    deploy: f8331ee9592927916fb8960fd8926975eb121888

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,7 +55,7 @@ const RESOURCES = {"bingequest/profile/index.html": "f928022699a4561b34b100061f1
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/canvaskit.js": "728b2d477d9b8c14593d4f9b82b484f3",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
-"flutter_bootstrap.js": "500a5ebc281344881e520d09d95d0db4",
+"flutter_bootstrap.js": "26152877322a7329976bd49ed7d17354",
 "manifest.json": "b903179230055979c0f8f0b0020c0492",
 "404.html": "d6f85ac73e83fb6cd6d44ca0e1f01557",
 "main.dart.js": "779d5ebeffed0e32e6a48942087fe4eb"};
