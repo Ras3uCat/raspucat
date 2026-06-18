326cc725bf735c1dd651dc8d849edf94a1ff2e6c
Ras3uCat <Ras3uCat@users.noreply.github.com>
2026-06-18 21:16:58 +0000

    deploy: d95b537dc4ccdaa30e8b77cc3979fb86bfdba190

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "963894683"
+    serviceWorkerVersion: "2136935687"
   }
 });
