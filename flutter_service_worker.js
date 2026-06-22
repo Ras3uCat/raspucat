d25467e968ae6f16a55210ae096b19b5de292081
Ras3uCat <Ras3uCat@users.noreply.github.com>
2026-06-22 19:25:01 +0000

    deploy: 8b4d536abb10a945641e92fe46b217cfabc1534e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"legal/bingequest/index.html": "29b2c6c54908d878e614bb4948dc8
 "icons/Icon-maskable-192.png": "980913cbf3e887c423c356b7d23428aa",
 "icons/Icon-192.png": "67c3c79c88cbde325b4a44796f45a27f",
 "404.html": "d6f85ac73e83fb6cd6d44ca0e1f01557",
-"flutter_bootstrap.js": "becc561ba057057aa77a17e7266c1184",
+"flutter_bootstrap.js": "027bb06df906f9eba9dfc22509b7d942",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
